c4fc44ccaf449cc8b9783707f60b3530ea8176b1
Marta Santacroce <82643850+marta01santacroce@users.noreply.github.com>
2025-03-05 09:40:33 +0100

    Add files via upload

--- try_llm_generator.py
+++ try_llm_generator.py
@@ -1,5 +1,5 @@
 import DB
-from search_v2 import hybrid_search
+import search_v2
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.schema import Document
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -8,9 +8,9 @@ from transformers import pipeline
 
 READER_MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
-
 tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)
+
 
 READER_LLM = pipeline(
     model=model,
@@ -20,29 +20,30 @@ READER_LLM = pipeline(
     temperature=0.2,
     repetition_penalty=1.1,
     return_full_text=False,
-    max_new_tokens=500,
+    max_new_tokens=1000,
 )
 
 
-# Prompt
+# Prompt con memoria
 prompt_in_chat_format = [
     {
         "role": "system",
-        "content": """Using the information contained in the context,give a comprehensive answer to the question.
-Respond only to the question asked, response should be concise and relevant to the question.
-Provide the number of the source document when relevant.
-If the answer cannot be deduced from the context, do not give an answer.""",
+        "content": """Using the information contained in the context and previous conversation history, give a comprehensive answer to the question.
+                    Respond only to the question asked, response should be concise and relevant to the question.
+                    Provide the number of the source document when relevant.
+                    If the answer cannot be deduced from the context, do not give an answer.""",
     },
     {
         "role": "user",
         "content": """Context:
-{context}
----
-Now here is the question you need to answer.
+        {context}
+        ---
+        Now here is the question you need to answer.
 
-Question: {question}""",
+        Question: {question}""",
     },
 ]
+
 RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
     prompt_in_chat_format, tokenize=False, add_generation_prompt=True
 )
@@ -57,8 +58,8 @@ def retrieve_documents(query):
     query_embedding = embedding_model.embed_query(formatted_query)
     query_embedding_str = "[" + ",".join(map(str, query_embedding)) + "]"
 
-    # Esegue la ricerca nel database (BM25 + Similarità Coseno)
-    results = hybrid_search(cursor, query, query_embedding_str)
+    # Esegue la ricerca nel database (BM25 + Similarità Coseno + reranking)
+    results = search_v2.hybrid_search(cursor, query, query_embedding_str)
 
     # Converte i risultati in oggetti Document di LangChain
     documents = [
@@ -104,11 +105,11 @@ if __name__ == "__main__":
 
         context += "".join([f"Document {str(i)}:::\n" + doc for i, doc in enumerate(retrieved_docs_text)])
 
+  
         if context:
-           
             final_prompt = RAG_PROMPT_TEMPLATE.format(question=query, context=context)
 
-            # Redact an answer
+            # Genera la risposta
             answer = READER_LLM(final_prompt)[0]["generated_text"]
             print(answer)
 
